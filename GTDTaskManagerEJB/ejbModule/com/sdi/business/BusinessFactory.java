b3cf10b5aa4d3a9b7059ca3cb4ba7458363f746b
JorgeZapa <kouuke9@gmail.com>
2017-04-11 16:34:13 +0200

    Añadido factories.properties para cambiar modo de deslieuge de forma dinámica

--- GTDTaskManagerEJB/ejbModule/com/sdi/business/BusinessFactory.java
+++ GTDTaskManagerEJB/ejbModule/com/sdi/business/BusinessFactory.java
@@ -3,7 +3,10 @@ package com.sdi.business;
 import com.sdi.business.services.Services;
 
 public class BusinessFactory {
+	private static String CONFIG_FILE = "/factories.properties";
 	
-	public static Services businessService = new LocalEjbServicesLocator();
+	public static Services businessService = (Services) FactoriesHelper.createFactory(CONFIG_FILE, "SERVICES_FACTORY");
+	
+	
 
 }
