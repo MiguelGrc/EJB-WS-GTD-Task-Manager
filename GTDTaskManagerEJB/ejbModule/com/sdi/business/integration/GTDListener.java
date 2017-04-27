ec113051c2e7ee28a7c8252f651c8b4a82d3dbac
JorgeZapa <kouuke9@gmail.com>
2017-04-27 21:41:23 +0200

    Cliente 50% Servidor 1% Messages

---
+++ GTDTaskManagerEJB/ejbModule/com/sdi/business/integration/GTDListener.java
@@ -0,0 +1,22 @@
+package com.sdi.business.integration;
+
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.ejb.ActivationConfigProperty;
+
+@MessageDriven(
+		activationConfig = {
+					@ActivationConfigProperty(
+								propertyName = "destination",
+								propertyValue = "queue/GTDQueue")
+})
+public class GTDListener implements MessageListener {
+
+	@Override
+	public void onMessage(Message arg0) {
+		System.out.println("GOTCHA!!");
+
+	}
+
+}
